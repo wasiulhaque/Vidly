de64d50a2714e96ba368cb65ad3ac96e76bc6d3e
wasiulhaque <wasiulhq@gmail.com>
2022-06-19 15:58:48 +0600

    Created random view

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek"
+            };
+            return View(movie);
+        }
+    }
+}
